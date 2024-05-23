c1d44bcaff0c6870b98b9d88674ce69e39636e7e
suzan-sheikh <mdsuzanskh@gmail.com>
2024-05-23 07:31:45 +0600

    .env file add

---
+++ clint.js
@@ -0,0 +1,14 @@
+VITE_APIKEY=AIzaSyC-WzDTlecXRF0tb0DXXPTWYIOdlNsH_NY
+VITE_AUTHDOMAIN=sta-vista.firebaseapp.com
+VITE_PROJECTID=sta-vista
+VITE_STORAGEBUCKET=sta-vista.appspot.com
+VITE_MESSAGINGSENDERID=564508446370
+VITE_APPID=1:564508446370:web:e4e86a73d65dba2af051fb
+
+
+VITE_IMGBB_API_KEY=c14a543e4fab6a67e7a3bb9c04924f62
+
+
+
+VITE_API_URL='http://localhost:8000'
+# VITE_API_URL='https://jobhunter-ten.vercel.app'
